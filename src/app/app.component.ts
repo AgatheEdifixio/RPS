0aaa81da89ef7863034f19c47f5e7e6a7e5fb834
AgatheEdifixio <agathe.marcellini@edifixio.com>
2021-07-10 12:31:21 +0200

    logic game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rock-paper';
+  public computerResult: string | undefined; 
+  public result: string | undefined;
+  private userResult: string | undefined; 
+
+ 
+
+  public play(action: string): void{
+    this.userResult = action;
+    this.computer();
+    this.calculateWinner();
+  }
+
+  private computer():void {
+    const randomNumber = Math.floor(Math.random() * 3);
+    const options: string[] = ['rock', 'paper', 'scissors'];
+    this.computerResult = options[randomNumber];
+  }
+
+  private calculateWinner():void {
+    console.log("USER choosed", this.userResult);
+    console.log("COMPUTER choosed", this.computerResult);
+    if (this.userResult === this.computerResult) {
+      this.result = 'There was a tie /draw';
+    }
+
+    if (this.userResult === 'rock' && this.computerResult === 'paper'){
+      this.result = 'Computer wins';
+    }
+
+    if (this.userResult === 'rock' && this.computerResult === 'scissors'){
+      this.result = 'You win';
+    }
+
+    if (this.userResult === 'paper' && this.computerResult === 'rock'){
+      this.result = 'You win';
+    }
+
+    if (this.userResult === 'paper' && this.computerResult === 'scissors'){
+      this.result = 'Computer wins';
+    }
+
+    if (this.userResult === 'scissors' && this.computerResult === 'rock'){
+      this.result = 'Computer wins';
+    }
+
+    if (this.userResult === 'scissors' && this.computerResult === 'paper'){
+      this.result = 'You win';
+    }
+  }
 }
